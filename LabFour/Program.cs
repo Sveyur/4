8a862e09169d4260042d1e16c989b799370def49
sveta <sveta@KOMP>
2021-11-21 03:02:23 +0300

    Добавьте файлы проекта.

---
+++ LabFour/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение N больше 0:  ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int N2 = 0;
+            int I = 1;
+            do
+            {
+                N2 += I;
+                I += 2;
+                Console.WriteLine(N2);
+            } while (2 * N - 1>= I);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LabFour_For/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabFour_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение N больше 0:  ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int n2 = 0, i=1; 2 * N - 1 >= i; )
+            {
+                n2 += i;
+                i += 2;
+                Console.WriteLine(n2);
+            }          
+           
+            Console.ReadKey();
+        }
+    }
+}
